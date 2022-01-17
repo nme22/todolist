91259455a0871e83b362e258ab7f2692711440cf
Navid Ebrahimi <navidmebrahimi22@gmail.com>
2022-01-17 08:51:03 -0500

    todoList component progress

--- components/TodoList.js
+++ components/TodoList.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { HStack, VStack, Text, IconButton } from '@chakra-ui/react';
+import { FaTrash } from 'react-icons/fa';
 
 function TodoList() {
-  return <div></div>;
+  const todos = [
+    {
+      id: 1,
+      body: 'read code',
+    },
+    {
+      id: 2,
+      body: 'practice code',
+    },
+  ];
+  return (
+    <VStack>
+      {todos.map((todo) => (
+        <HStack>
+          <Text>{todo.body}</Text>
+          <IconButton icon={<FaTrash />} />
+        </HStack>
+      ))}
+    </VStack>
+  );
 }
 
 export default TodoList;
--- pages/index.js
+++ pages/index.js
@@ -22,6 +22,7 @@ export default function Home() {
       >
         Todo Application
       </Heading>
+      <TodoList />
     </VStack>
   );
 }
